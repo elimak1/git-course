6fa57788158f74e76d9af9d03ee02fd6c7af5c69
elimak1 <elias.makela123@gmail.com>
2020-05-13 20:59:43 +0300

    realising branching task

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,3 @@
-
-def main():
-    print("Hello World!")
-    print("Hello from feature")
-    print("Hello from release")
-
-main()
-
+print("Hello World!")
+print("Hello from feature")
+print("Hello from release")
